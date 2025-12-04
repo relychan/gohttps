1478a287f769832ea82644b1c6ea90fa0270bb2d
Toan Nguyen <hgiasac@gmail.com>
2025-12-04 12:27:30 +0700

    fix jsonschema duration regex

--- jsonschema/generate.go
+++ jsonschema/generate.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/invopop/jsonschema"
 	"github.com/relychan/gohttps"
+	"github.com/relychan/goutils"
 )
 
 func main() {
@@ -36,8 +37,10 @@ func jsonSchemaConfiguration() error {
 	reflectSchema.Definitions["Duration"] = &jsonschema.Schema{
 		Type:        "string",
 		Description: "Duration string",
-		Pattern:     "^((([0-9]+h)?([0-9]+m)?([0-9]+s))|(([0-9]+h)?([0-9]+m))|([0-9]+h))$",
+		MinLength:   goutils.ToPtr(uint64(2)),
+		Pattern:     `^(-?\d+(\.\d+)?h)?(-?\d+(\.\d+)?m)?(-?\d+(\.\d+)?s)?(-?\d+(\.\d+)?ms)?$`,
 	}
+
 	reflectSchema.Definitions["ServerConfig"].Properties.Set("requestTimeout", &jsonschema.Schema{
 		Description: "The default timeout of every request. Return a 504 Gateway Timeout error to the client.",
 		Ref:         "#/$defs/Duration",
@@ -46,10 +49,15 @@ func jsonSchemaConfiguration() error {
 		Description: "The maximum duration for reading the entire request, including the body.\nA zero or negative value means there will be no timeout.",
 		Ref:         "#/$defs/Duration",
 	})
-	reflectSchema.Definitions["ServerConfig"].Properties.Set("readHeaderTimeout", &jsonschema.Schema{
-		Description: "The amount of time allowed to read request headers.\nThe connection's read deadline is reset after reading the headers and the Handler can decide what is considered too slow for the body.\nIf zero, the value of ReadTimeout is used. If negative, or if zero and ReadTimeout is zero or negative, there is no timeout.",
-		Ref:         "#/$defs/Duration",
-	})
+	reflectSchema.Definitions["ServerConfig"].Properties.Set(
+		"readHeaderTimeout",
+		&jsonschema.Schema{
+			Description: `The amount of time allowed to read request headers.
+The connection's read deadline is reset after reading the headers and the Handler can decide what is considered too slow for the body.
+If zero, the value of ReadTimeout is used. If negative, or if zero and ReadTimeout is zero or negative, there is no timeout.`,
+			Ref: "#/$defs/Duration",
+		},
+	)
 	reflectSchema.Definitions["ServerConfig"].Properties.Set("writeTimeout", &jsonschema.Schema{
 		Description: "The maximum duration before timing out writes of the response. It is reset whenever a new request's header is read.\nLike ReadTimeout, it does not let Handlers make decisions on a per-request basis.\nA zero or negative value means there will be no timeout.",
 		Ref:         "#/$defs/Duration",
